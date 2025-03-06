a6dbe24ba26e608b457dd3f0a2d60ba889b24a08
Sanjo Fumihiro <sanjofumihiro@gmail.com>
2025-03-07 05:11:07 +0900

    動いた

--- strider_package/adjust.py
+++ strider_package/adjust.py
@@ -2,7 +2,173 @@ import rclpy
 from rclpy.node import Node
 from kk_driver_msg.msg import PwmCmd  # カスタムメッセージのインポート
 import threading
+import math
+import time
 
+import numpy as np
+
+
+
+class StriderWalkIK():
+    # 初期化
+    def __init__(self, node):
+        self.MIN_CALC = 0.001;
+        self.MIN_DIST = 0.001;
+        self.ROOT = 0.5;
+        self.LEG = 0.5;
+        self.node = node;
+        self.is_run = False
+        
+        self.last_x = [0, 0, 0, 0]
+
+        idx_max = 1 / self.MIN_DIST;
+        self.distable = [0] * int(idx_max);
+        for rad in np.arange(0.0, math.pi, self.MIN_CALC):
+            dist = math.sqrt(self.ROOT * self.ROOT + self.LEG * self.LEG - 2 * self.LEG * self.ROOT * math.cos(rad))
+            for widx in range(int(self.dist_idx(dist)), int(idx_max), 1):
+                self.distable[widx] = rad;
+        print(self.distable)
+    # 機構メッセージのPublish
+    def publishCmd(self, msg):
+        rev = [-1, 1, 1, -1, -1, 1, 1, -1]
+        common_center = 750
+        center = [700, 700, 700, 710, 650, 700, 500, 720]
+        # center = [700, 700, 700, 710, 750, 700, 600, 720]
+        self.node.get_logger().info(f'Published(bef adjust): {msg}')
+        for idx in range(len(msg.port)):
+            adjust = center[msg.port[idx]] - common_center;
+            pos = (msg.pos[idx] - common_center + adjust) * rev[msg.port[idx]] + common_center
+            if (pos < 0):
+                pos = 0
+            msg.pos[idx] = pos
+        self.node.publisher_.publish(msg)
+    # 距離テーブルインデックス算出
+    def dist_idx(self,dist):
+        idx = int(dist / self.MIN_DIST);
+        if (len(self.distable) <= idx):
+            idx = len(self.distable) - 1;
+        return idx;
+    # 足逆運動学計算
+    def calc(self, x, y):
+
+        tmp_dist = math.sqrt(x * x + y * y)
+        tmp_rad = math.atan2(y, -x)
+        tmp_rad_leg = self.distable[self.dist_idx(tmp_dist)]
+        rad_root = tmp_rad - (math.pi - tmp_rad_leg) / 2
+        rad_leg = math.pi - tmp_rad_leg + rad_root
+        # print(rad_root / math.pi  * 180, rad_leg / math.pi  * 180)
+
+        return rad_root, rad_leg
+    # Duty変換
+    def rad2Duty(self, rad):
+        pi_duty = 900
+        half_duty = 750;
+        return int(pi_duty * (rad / math.pi - 1/2) + half_duty)
+    # 機構破損防止計算
+    def checkBreak(self, radroot, radleg):
+        if (radroot > radleg):
+            print("Axis Broken!!", radroot, radleg);
+            return False
+        return True
+    # 機構動作
+    def mov(self, idx, x, y, speed = 7):
+        self.last_x[idx] = x
+        root, leg = self.calc(x,y)
+        if (self.checkBreak(x, y)):
+            msg = PwmCmd()
+            msg.child_id = 0
+            msg.port = [idx * 2, idx * 2 + 1]
+            msg.pos = [self.rad2Duty(leg), self.rad2Duty(root)]
+            msg.spd = [speed, speed]
+            self.publishCmd(msg)
+            return True
+        return False
+        
+    def movUP(self, idx, x, y, speed = 7):
+        self.last_x[idx] = x
+        root, leg = self.calc(x,y)
+        if (self.checkBreak(x, y)):
+            msg = PwmCmd()
+            msg.child_id = 0
+            msg.port = [idx * 2, idx * 2 + 1]
+            msg.pos = [self.rad2Duty(3.1), self.rad2Duty(root)]
+            msg.spd = [speed, speed]
+            self.publishCmd(msg)
+            return True
+        return False
+    
+    def start(self):        
+        self.jump_idx = [1.0, 1.0, 1.0, 1.0]
+        self.base_y = 0.9
+        self.jump_y = 0.4
+        self.base_x = 0
+        self.grand_x = [0.35, 0.05, 0.2, -0.1]
+        self.x_offset = [ -0.15, -0.15, 0, 0]
+        self.y_offset = [-0.1, -0.05, -0.05, 0]
+
+        self.LIMIT_X = -0.2
+        self.START_X = 0.4
+        self.UPDATE_X = -0.01
+
+        self.UPDATE_JUMP = 0.1
+    def update(self):
+        self.run = True;
+        self.first = True;
+        self.start();
+
+        start_x = 0.5;
+        finish_x = -0.5
+        base_y = 0.85
+        jump_y = 0.5
+        jump_mode  = [False, False, False, False]
+
+        all_speed = 1
+        control_hz = 10;
+        jump_sec = 0.2;
+        jump_wait = 0.2;
+        const_jump_delay = 0.1;
+        sequence_sec = 4;
+        sequence_count = 0;
+
+        sequence_full = sequence_sec * control_hz;
+        jump_full= jump_sec * control_hz
+        sequence_offset = [0] * 4
+        sequence_offset[0] = 0;
+        sequence_offset[1] = sequence_full / 2;
+        sequence_offset[2] =  jump_full + const_jump_delay * sequence_full;
+        sequence_offset[3] = sequence_full / 2 + jump_full + const_jump_delay * sequence_full;
+
+        while(self.run):
+            time.sleep(1/ all_speed / control_hz);
+            if not (self.is_run or self.first):
+                continue;
+            self.first = False;
+            sequence_count = sequence_count + 1;
+            for idx in range(4):
+                frame_count = (sequence_count + sequence_offset[idx]) % sequence_full
+                if (frame_count < jump_full):
+                    x = (start_x - finish_x) / jump_full * frame_count + finish_x  + self.x_offset[idx];
+
+                    if (jump_mode[idx]):
+                        # self.mov(idx, x, jump_y + self.y_offset[idx], 11);
+                        self.movUP(idx, start_x + self.x_offset[idx], base_y + self.y_offset[idx], 15);
+                    else:
+                        self.mov(idx, finish_x , jump_y, 15);
+                        time.sleep(0.15)
+                        self.movUP(idx, finish_x + self.x_offset[idx], jump_y + self.y_offset[idx], 15);
+                        time.sleep(0.1)
+                    jump_mode[idx] = True;
+                else:
+                    x = (finish_x - start_x) / (sequence_full - jump_full) * (frame_count - jump_full) + start_x  + self.x_offset[idx];
+                    
+                    if (jump_mode[idx]):
+                        self.movUP(idx, start_x + self.x_offset[idx], base_y + self.y_offset[idx], 15);
+                        time.sleep(jump_wait)
+                        self.mov(idx, start_x, base_y + self.y_offset[idx], 8);
+                        time.sleep(jump_wait)
+                    else:
+                        self.mov(idx, x, base_y + self.y_offset[idx], 11);
+                    jump_mode[idx] = False;
 
 
 class PwmCmdPublisher(Node):
@@ -11,21 +177,7 @@ class PwmCmdPublisher(Node):
         self.publisher_ = self.create_publisher(PwmCmd, '/pwm/cmd', 8)
         # self.timer = self.create_timer(0.1, self.publish_pwm_cmd)
         self.get_logger().info('PWM Cmd Publisher Node has been started.')
-
-    def publishCmd(self, msg):
-        rev = [-1, 1,-1, -1, -1, 1, 1, -1]
-        common_center = 750
-        center = [750, 750, 750, 750, 750, 750, 650, 750]
-        self.get_logger().info(f'Published(bef adjust): {msg}')
-        for idx in range(len(msg.port)):
-            adjust = center[msg.port[idx]] - common_center;
-            pos = (msg.pos[idx] - common_center + adjust) * rev[msg.port[idx]] + common_center
-            if (pos < 0):
-                pos = 0
-            msg.pos[idx] = pos
-        self.publisher_.publish(msg)
-
-
+        self.IK = StriderWalkIK(self)
 
     def publish_All(self):
         # メッセージの作成とデータ設定
@@ -35,7 +187,7 @@ class PwmCmdPublisher(Node):
         msg.port = [0, 1, 2, 3, 4, 5, 6, 7]  # 複数のポートを設定
         msg.pos = [750, 750 + duty, 750, 750 - duty, 750, 750+ duty, 750, 750 - duty]  # 各ポートに対応するポジション
         msg.spd = [7, 7, 7, 7, 7, 7, 7, 7]  # 各ポートに対応する速度
-        self.publishCmd(msg)
+        self.IK.publishCmd(msg)
     
     def publishDown(self):
         # メッセージの作成とデータ設定
@@ -44,7 +196,7 @@ class PwmCmdPublisher(Node):
         msg.port = [0, 1, 2, 3, 4, 5, 6, 7]  # 複数のポートを設定
         msg.pos = [1200, 300, 1200, 300, 1200, 300, 1200, 300]  # 各ポートに対応するポジション
         msg.spd = [7, 7, 7, 7, 7, 7, 7, 7]  # 各ポートに対応する速度
-        self.publishCmd(msg)
+        self.IK.publishCmd(msg)
 
     def publishCenter(self):
         # メッセージの作成とデータ設定
@@ -53,7 +205,7 @@ class PwmCmdPublisher(Node):
         msg.port = [0, 1, 2, 3, 4, 5, 6, 7]  # 複数のポートを設定
         msg.pos = [750, 750, 750, 750, 750, 750, 750, 750]  # 各ポートに対応するポジション
         msg.spd = [7, 7, 7, 7, 7, 7, 7, 7]  # 各ポートに対応する速度
-        self.publishCmd(msg)
+        self.IK.publishCmd(msg)
 
     def publishUp(self):
         # メッセージの作成とデータ設定
@@ -61,10 +213,12 @@ class PwmCmdPublisher(Node):
         msg.child_id = 0  # 任意の値を設定
         msg.port = [0, 1, 2, 3, 4, 5, 6, 7]  # 複数のポートを設定
         msg.pos =  [900, 600, 900, 600, 900, 600, 900, 600]  # 各ポートに対応するポジション
-        msg.spd = [7, 7, 7, 7, 7, 7, 7, 7]  # 各ポートに対応する速度
-        self.publishCmd(msg)
+        msg.spd = [6, 6, 6, 6, 6, 6, 6, 6]  # 各ポートに対応する速度
+        self.IK.publishCmd(msg)
 
     def publish_Once(self):
+        
+        self.IK.update();
         cmd = input("ポートかコマンドを指定してください")
         if (cmd == "r"):
             self.publishUp()
@@ -76,26 +230,48 @@ class PwmCmdPublisher(Node):
             self.publish_All()
         elif (cmd == "c"):
             self.publishCenter()
+        elif (cmd == "x"):
+            x = int(input("x座標"))
+            y = int(input("y座標"))
+            self.IK.mov(0, x, y)
+        elif (cmd == "p"):
+            self.IK.update();
         else:
-            port = int(cmd)
-            duty = int(input("Dutyを指定してください(1-180)"))
-            msg = PwmCmd()
-            msg.child_id = 0  # 任意の値を設定
-            msg.port = [port]  # 複数のポートを設定
-            msg.pos = [int(duty)]  # 各ポートに対応するポジション
-            msg.spd = [15]  # 各ポートに対応する速度
-            # メッセージのパブリッシュ
-            self.publishCmd(msg)
+            mov_x = float(input("xを指定してください(1-180)"))
+            mov_y = float(input("yを指定してください(1-180)"))
+            self.IK.mov(int(cmd), mov_x, mov_y)
+
+            # port = int(cmd)
+            # duty = int(input("Dutyを指定してください(1-180)"))
+            # msg = PwmCmd()
+            # msg.child_id = 0  # 任意の値を設定
+            # msg.port = [port]  # 複数のポートを設定
+            # msg.pos = [int(duty)]  # 各ポートに対応するポジション
+            # msg.spd = [15]  # 各ポートに対応する速度
+            # # メッセージのパブリッシュ
+            # self.IK.publishCmd(msg)
     def loop(self):
         self.publishDown()
         while(True):
             self.publish_Once()
 
+    def ctrl_th(self):
+        self.run_flag = False
+        input("Enterで動作開始します")
+        self.IK.is_run = False
+        while(True):
+            input("q:Stop, Enter")
+            self.IK.is_run = True
+            input("")
+            self.IK.is_run = False
+
 def main(args=None):
     rclpy.init(args=args)
     node = PwmCmdPublisher()
     node_loop = threading.Thread(target=node.loop)
+    ctrl_loop = threading.Thread(target=node.ctrl_th)
     node_loop.start()
+    ctrl_loop.start()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
